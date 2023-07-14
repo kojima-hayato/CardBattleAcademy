e2a798d0470b8beb7d56ffc315962c7d8117524c
kojimahayato <x20u019@chiba-fjb.ac.jp>
2023-07-14 12:24:09 +0900

    7/14 戦闘画面

--- Assets/Scripts/BattleManager.cs
+++ Assets/Scripts/BattleManager.cs
@@ -39,21 +39,31 @@ public class BattleManager : MonoBehaviour
     public GameObject timeText;
 
     //プレイヤー情報
+    string playerName;
     int playerLv;
-    int playerHp;
+    int playerHp = 12;
     int playerSp;
-    int playerAtk;
-    int playerDef;
+    int playerAtk = 5;
+    int playerDef = 2;
     int playerSpeed;
 
     //モンスター情報
-    int monsterHp;
+    string monsterName = "スライム";
+    int monsterHp = 5;
     int monsterSp;
-    int monsterAtk;
-    int monsterDef;
+    int monsterAtk = 4;
+    int monsterDef = 2;
     int monsterSpeed;
 
+    //問題ターンか
     bool isQuestionTurn;
+
+    //タイマー
+    float leftTime = 15;
+    float timer;
+
+    GameObject clickedGameObject;
+
 
     // Start is called before the first frame update
     void Start()
@@ -64,22 +74,92 @@ public class BattleManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //問題ターンか
+        if (isQuestionTurn)
+        {
+            
+            //制限時間内か
+            if (leftTime >= 0)
+            {
+                //カウントダウン
+                leftTime -= Time.deltaTime;
+                timeText.GetComponent<Text>().text = leftTime.ToString("f1");
+                timeBar.GetComponent<Slider>().value = leftTime;
+            }
+            if (Input.GetMouseButtonDown(0))
+            {
+                //選択肢をクリック
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                RaycastHit2D hit2D = Physics2D.Raycast((Vector2)ray.origin, (Vector2)ray.direction);
+
+                if (hit2D)
+                {
+                    isQuestionTurn = false;
+                    ChangePhase();
+                    clickedGameObject = hit2D.transform.gameObject;
+                    //正解か不正解か
+                    if(clickedGameObject.tag == "true")
+                    {
+                        Debug.Log("正解");
+                        //ダメージ計算
+                        int damage = playerAtk - monsterDef;
+                        messageText.GetComponent<Text>().text = "正解!" + monsterName + "に" + damage + "のダメージを与えた";
+                        monsterHpBar.GetComponent<Slider>().value = monsterHp - damage;
+                    }
+                    else
+                    {
+                        Debug.Log("不正解");
+                        messageText.GetComponent<Text>().text = "不正解!";
+                    }
+                    StartCoroutine("Interval");
+                    MonsterTurn();
+                }
+            }
+        }
+    }
+
+    void MonsterTurn()
+    {
+        int damage = monsterAtk - playerDef;
+        messageText.GetComponent<Text>().text = "スライムの攻撃!" + damage + "のダメージを受けた";
+        playerHpBar.GetComponent<Slider>().value = playerHp - damage;
+        StartCoroutine("Interval");
+        SetQuestion();
+        ChangePhase();
     }
 
     void SetQuestion()
     {
+        //問題文と選択肢
         questionText.GetComponent<Text>().text = "人の不注意に付け込んで機密情報などを不正に入手する手法は？";
         choiceText1.GetComponent<Text>().text = "SQLインジェクション";
         choiceText2.GetComponent<Text>().text = "ソーシャルエンジニアリング";
         choiceText3.GetComponent<Text>().text = "DDOS攻撃";
         choiceText4.GetComponent<Text>().text = "バッファオーバーフロー";
+
+        //正答セット
+        choiceWindow1.tag = "false";
+        choiceWindow2.tag = "true";
+        choiceWindow3.tag = "false";
+        choiceWindow4.tag = "false";
     }
+
+    IEnumerator Interval()
+    {
+        //2秒待つ
+        yield return new WaitForSeconds(2);
+    }
+
+ 
 
     IEnumerator StartBattle()
     {
         //2秒待つ
         yield return new WaitForSeconds(2);
+        playerHpBar.GetComponent<Slider>().maxValue = playerHp;
+        playerHpBar.GetComponent<Slider>().value = playerHp;
+        monsterHpBar.GetComponent<Slider>().maxValue = monsterHp;
+        monsterHpBar.GetComponent<Slider>().value = monsterHp;
         isQuestionTurn = true;
         SetQuestion();
         ChangePhase();
@@ -129,6 +209,4 @@ public class BattleManager : MonoBehaviour
             messageWindow.SetActive(true);
         }
     }
-
-
 }
