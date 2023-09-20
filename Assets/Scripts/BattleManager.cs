d75ac255ad59f5194d795bea3eb12efc5634299c
kojima-hayato <103411394+kojima-hayato@users.noreply.github.com>
2023-09-20 12:40:51 +0900

    9/20

--- Assets/Scripts/BattleManager.cs
+++ Assets/Scripts/BattleManager.cs
@@ -35,6 +35,18 @@ public class BattleManager : MonoBehaviour
     public GameObject skillText;
     public GameObject itemText;
     public GameObject escapeText;
+    int skillAct = 1;
+
+    //スキルウィンドウ
+    public GameObject skillWindow;
+
+    //スキルテキスト
+    public GameObject skillText1;
+    public GameObject skillText2;
+    public GameObject skillText3;
+    public GameObject skillText4;
+    public GameObject skillText5;
+    public GameObject skillText6;
 
     //サウンド
     public GameObject BGM;
@@ -66,9 +78,10 @@ public class BattleManager : MonoBehaviour
     int monsterDef = 2;
     int monsterSpeed;
 
-    //問題ターンか
+    //ターン判定
     bool isQuestionTurn;
     bool isActTurn;
+    bool isSkillTurn;
 
     //タイマー
     float remainTime = 15;
@@ -119,12 +132,64 @@ public class BattleManager : MonoBehaviour
                 act++;
                 actSelect.transform.Translate(0.65f,0,0);
             }
-            if(Input.GetKeyDown(KeyCode.Return) && act == 1)
+            if(Input.GetKeyDown(KeyCode.Return))
             {
-                isActTurn = false;
-                isQuestionTurn = true;
-                SetQuestion();
-                ChangeTurn();
+                //たたかう
+                if(act == 1)
+                {
+                    isActTurn = false;
+                    isQuestionTurn = true;
+                    SetQuestion();
+                    ChangeTurn();
+                }
+                //スキル
+                else if(act == 2)
+                {
+                    isActTurn = false;
+                    isSkillTurn = true;
+                    SkillActive();
+                }
+                //アイテム
+                else if(act == 3)
+                {
+
+                }
+                //逃げる
+                else if(act == 4)
+                {
+                    isActTurn = false;
+                    ChangeTurn();
+                    StartCoroutine("EndBattle");
+                }
+            }
+        }
+
+        //スキルターンか
+        if (isSkillTurn)
+        {
+            if (Input.GetKeyDown(KeyCode.W) && skillAct != 1 && skillAct != 4)
+            {
+                //矢印を動かす
+                skillAct--;
+                actSelect.transform.Translate(-0.65f, 0, 0);
+            }
+            if (Input.GetKeyDown(KeyCode.S) && skillAct != 3 && skillAct != 6)
+            {
+                //矢印を動かす
+                skillAct++;
+                actSelect.transform.Translate(0.65f, 0, 0);
+            }
+            if (Input.GetKeyDown(KeyCode.A) && skillAct != 1 && skillAct != 2 && skillAct != 3)
+            {
+                //矢印を動かす
+                skillAct -= 3;
+                actSelect.transform.Translate(0, 2, 0);
+            }
+            if (Input.GetKeyDown(KeyCode.D) && skillAct != 4 && skillAct != 5 && skillAct != 6)
+            {
+                //矢印を動かす
+                skillAct += 3;
+                actSelect.transform.Translate(0, 2, 0);
             }
         }
 
@@ -186,9 +251,7 @@ public class BattleManager : MonoBehaviour
                     //戦闘終了判定
                     if (monsterHp <= 0)
                     {
-                        isActTurn = false;
-                        isQuestionTurn = false;
-                        EndBattle();
+                        StartCoroutine("EndBattle");
                     }
                     else
                     {
@@ -218,9 +281,7 @@ public class BattleManager : MonoBehaviour
         //戦闘終了判定
         if (playerHp <= 0)
         {
-            isActTurn = false;
-            isQuestionTurn = false;
-            EndBattle();
+            StartCoroutine("EndBattle");
         }
         else
         {
@@ -231,15 +292,20 @@ public class BattleManager : MonoBehaviour
         }
     }
 
+    //戦闘終了
     IEnumerator EndBattle()
     {
-        if(playerHp == 0)
+        if(monsterHp <= 0)
         {
             messageText.GetComponent<Text>().text = "敵を倒した！";
         }
-        else
+        else if(playerHp <= 0)
         {
             messageText.GetComponent<Text>().text = "全滅した...";
+        }
+        else
+        {
+            messageText.GetComponent<Text>().text = "逃げ出した！";
         }
         //2秒待つ
         yield return new WaitForSeconds(2);
@@ -261,8 +327,7 @@ public class BattleManager : MonoBehaviour
         choiceWindow3.tag = "false";
         choiceWindow4.tag = "false";
 
-        //制限時間リセット
-        remainTime = 15;
+        
     }
 
     //問題ターンとその他ターンの変更
@@ -283,6 +348,7 @@ public class BattleManager : MonoBehaviour
             questionWindow.SetActive(true);
             timeText.SetActive(true);
             timeBar.SetActive(true);
+            remainTime = 15;
 
             //メッセージウィンドウを消す
             messageText.SetActive(false);
@@ -331,6 +397,40 @@ public class BattleManager : MonoBehaviour
             skillText.SetActive(false);
             itemText.SetActive(false);
             escapeText.SetActive(false);
+        }
+    }
+    void SkillActive()
+    {
+        if (isSkillTurn)
+        {
+            skillWindow.SetActive(true);
+            skillText1.SetActive(true);
+            skillText2.SetActive(true);
+            skillText3.SetActive(true);
+            skillText4.SetActive(true);
+            skillText5.SetActive(true);
+            skillText6.SetActive(true);
+            actWindow.SetActive(false);
+            fightText.SetActive(false);
+            skillText.SetActive(false);
+            itemText.SetActive(false);
+            escapeText.SetActive(false);
+            actSelect.transform.position = new Vector3(-5, -2, 0);
+        }
+        else
+        {
+            skillWindow.SetActive(false);
+            skillText1.SetActive(false);
+            skillText2.SetActive(false);
+            skillText3.SetActive(false);
+            skillText4.SetActive(false);
+            skillText5.SetActive(false);
+            skillText6.SetActive(false);
+            actWindow.SetActive(true);
+            fightText.SetActive(true);
+            skillText.SetActive(true);
+            itemText.SetActive(true);
+            escapeText.SetActive(true);
         }
     }
 }
