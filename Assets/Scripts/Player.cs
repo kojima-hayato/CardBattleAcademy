cfa9fe0b0e6b540bfe203af7b3236cf8747b64e4
miura-zento <103411413+miura-zento@users.noreply.github.com>
2023-09-29 11:06:56 +0900

    キャラクターの移動アニメーション設定

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,7 +2,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
- 
+
+
+public enum Direction
+{
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
 public class Player : MonoBehaviour
 {
     [Range(0, 2)] public float MoveSecond = 0.1f;
@@ -71,4 +80,58 @@ public class Player : MonoBehaviour
             transform.position = RPGSceneManager.ActiveMap.Grid.CellToWorld(Pos);
         }
     }
+
+
+
+    [SerializeField] Direction _currentDir = Direction.Down;
+    public Direction CurrentDir
+    {
+        get => _currentDir;
+        set
+        {
+            if (_currentDir == value) return;
+            _currentDir = value;
+            SetDirAnimation(value);
+        }
+    }
+    public void SetDir(Vector3Int move)
+    {
+        if (Mathf.Abs(move.x) > Mathf.Abs(move.y))
+        {
+            CurrentDir = move.x > 0 ? Direction.Right : Direction.Left;
+        }
+        else
+        {
+            CurrentDir = move.y > 0 ? Direction.Up : Direction.Down;
+        }
+    }
+
+    Animator Animator { get => GetComponent<Animator>(); }
+    static readonly string TRIGGER_MoveDown = "MoveDownTrigger";
+    static readonly string TRIGGER_MoveLeft = "MoveLeftTrigger";
+    static readonly string TRIGGER_MoveRight = "MoveRightTrigger";
+    static readonly string TRIGGER_MoveUp = "MoveUpTrigger";
+
+    void SetDirAnimation(Direction dir)
+    {
+        if (Animator == null || Animator.runtimeAnimatorController == null) return;
+
+        string triggerName = null;
+        switch (dir)
+        {
+            case Direction.Up: triggerName = TRIGGER_MoveUp; break;
+            case Direction.Down: triggerName = TRIGGER_MoveDown; break;
+            case Direction.Left: triggerName = TRIGGER_MoveLeft; break;
+            case Direction.Right: triggerName = TRIGGER_MoveRight; break;
+            default: throw new System.NotImplementedException("");
+        }
+        Animator.SetTrigger(triggerName);
+    }
+
+    private void Awake()
+    {
+        SetDirAnimation(_currentDir);
+    }
 }
+
+
