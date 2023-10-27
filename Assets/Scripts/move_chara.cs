d0262019bdfd7b7d11cbc19b4bfcf771ed51e6a8
miura-zento <103411413+miura-zento@users.noreply.github.com>
2023-10-27 12:40:06 +0900

    速度調節

--- Assets/Scripts/move_chara.cs
+++ Assets/Scripts/move_chara.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 
 public class move_chara: MonoBehaviour
 {
-    private float speed = 0.005f; //float‚Í¬”“_
+    private float speed = 0.011f; //float‚Í¬”“_
     private Animator animator;
 
     private void Start()
