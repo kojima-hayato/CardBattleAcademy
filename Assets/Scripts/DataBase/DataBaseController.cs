7e39a4a63ee86c7ff4ff83d74548f6edab862e05
kabe-fumiya <103411418+kabe-fumiya@users.noreply.github.com>
2023-11-02 14:12:12 +0900

    DB構築できた！
    やったー！
    サーバ構築したら宛先変える必要があります。
    サーバを立ててそこを共有するか、現在の仮置きを共有して使用するか検討が必要です。

---
+++ Assets/Scripts/DataBase/DataBaseController.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MySql.Data.MySqlClient;
+
+public class DataBaseController : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
