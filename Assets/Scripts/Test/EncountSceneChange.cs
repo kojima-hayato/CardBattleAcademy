331ba95986c3fde7161b02eb1a3fc3e1ef5ec8a6
miura-zento <103411413+miura-zento@users.noreply.github.com>
2023-11-21 18:24:48 +0900

    宝箱アニメーション
    失敗！！

--- Assets/Scripts/Test/EncountSceneChange.cs
+++ Assets/Scripts/Test/EncountSceneChange.cs
@@ -2,32 +2,39 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using System.Collections;
 
-
 public class EncountSceneChange : MonoBehaviour
 {
     public string BossBattle;
+    public ConversationScript conversationScript;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            Debug.Log("ここから非同期シーン読み込み");
-            StartCoroutine(LoadBossBattleAsync());
+            Debug.Log("エンカウントエリアに入りました");
+            StartCoroutine(LoadBossBattleAndDisplayConversation());
         }
     }
 
-    IEnumerator LoadBossBattleAsync()
+    IEnumerator LoadBossBattleAndDisplayConversation()
     {
+        Debug.Log("ボスバトルシーンの読み込みを開始します");
         AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(BossBattle);
+        asyncLoad.allowSceneActivation = false; // シーン遷移を一時停止
 
-        // シーンの読み込みが完了するまで待機
+        // 会話ログの制御を開始
+        if (conversationScript != null)
+        {
+            yield return StartCoroutine(conversationScript.StartConversation()); // 会話が終了するまで待つ
+        }
+
+        Debug.Log("会話が終了したため、シーン遷移を開始します");
+        asyncLoad.allowSceneActivation = true; // シーン遷移を許可
+
         while (!asyncLoad.isDone)
         {
-            float progress = Mathf.Clamp01(asyncLoad.progress / 0.9f);
-            Debug.Log("シーン読み込み中... " + (progress * 100f) + "%");
             yield return null;
         }
-
-        Debug.Log("BossBattleシーン読み込み完了");
     }
+
 }
